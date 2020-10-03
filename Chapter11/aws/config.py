1d5e697eb2d3c1d00c945cb339e686bbbae9595d
aisolab <bsk0130@gmail.com>
2020-10-03 15:15:14 +0900

    fixed typo

--- Chapter11/aws/config.py
+++ Chapter11/aws/config.py
@@ -28,7 +28,7 @@ test_config = {
     'JWT_SECRET_KEY': 'SOME_SUPER_SECRET_KEY',
     'JWT_EXP_DELTA_SECONDS': 7 * 24 * 60 * 60,
     'S3_BUCKET': "test",
-    'S3_ACCESS_KEY': "test_acces_key",
+    'S3_ACCESS_KEY': "test_access_key",
     'S3_SECRET_KEY': "test_secret_key",
     'S3_BUCKET_URL': f"https://s3.ap-northeast-2.amazonaws.com/test/"
 }
